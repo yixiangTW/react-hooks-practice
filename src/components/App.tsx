98570c2d1b6bd2bced7817e7f5f8b52f20d87e04
Liuyixiang <yixiang.liu@thoughtworks.com>
2022-02-21 15:02:42 +0800

    Modify title

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -38,7 +38,7 @@ export default function App () {
 
   return (
     <Provider store={{ state, dispatch }}>
-      <div>todo list aaa</div>
+      <div>Todo</div>
       <button onClick={onAdd}>add</button>
       <input ref={ref}/>
       <TodoList todoList={ state.todoList } onRemove={onRemove}/>
